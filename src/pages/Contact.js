e3896cf618d479fca4c10df6db7e80932df076f2
mikeyp957 <peraram2@gmail.com>
2021-06-27 22:08:21 -0700

    contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Title from '../components/Title'
+import Card from 'react-bootstrap/Card'
+import '../App.css'
+
+
+function Contact() {
+return (
+    <div>
+
+    <Card 
+    className="contactCard">
+         <Title title={'Contact Information'} />
+        <h5>Email: peraram2@gmail.com</h5>
+        
+        <h5>Github: <a href="https://github.com/MikeyP957">github.com/MikeyP957</a> </h5>
+
+
+        <h5>linkdin: <a href="https://www.linkedin.com/in/mikeyp957/">linkedin.com/in/mikeyp957/
+            </a></h5>
+    </Card>
+        
+
+    </div>
+)
+}
+
+export default Contact
