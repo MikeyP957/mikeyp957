f66b6f55e0199a5e27c4ca538e8eefcf8c95da1b
mikeyp957 <peraram2@gmail.com>
2021-06-27 22:06:13 -0700

    app routed and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
+import Nav from './components/Nav'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/Home"]}>
+              <Home />
+          </Route>
+          <Route exact path={["/", "/Portfolio"]}>
+              <Portfolio />
+          </Route>
+          <Route exact path={["/", "/Contact"]}>
+              <Contact />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+
   );
 }
 
 export default App;
+
