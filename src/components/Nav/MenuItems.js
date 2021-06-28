15ef113d72983935a57c657782ba786e24bc77b1
mikeyp957 <peraram2@gmail.com>
2021-06-27 22:06:45 -0700

    navbar created

---
+++ src/components/Nav/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '/home',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Portfolio',
+        url: '/portfolio',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '/contact',
+        cName: 'nav-links'
+    },
+]
