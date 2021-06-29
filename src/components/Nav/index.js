37335c1545b750fd694e088831242fbe8edd5827
mikeyp957 <peraram2@gmail.com>
2021-06-29 12:23:58 -0700

    add font awesome, change a tag to link compt

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { MenuItems } from './MenuItems'
- import './Navbar.css'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 import 'react-bootstrap'
 
 
@@ -25,9 +26,9 @@ class Navbar extends Component {
           {MenuItems.map((item, index) => {
             return(
               <li key={index}> 
-                <a className= {item.cName} href = {item.url}>
+                <Link className= {item.cName} to = {item.url}>
                   {item.title}
-                </a> 
+                </Link> 
               </li>
             )
           })}          
