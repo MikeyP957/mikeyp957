1d9496b43be8e51372e7cba508cf0cf7852928e9
mikeyp957 <peraram2@gmail.com>
2021-07-05 15:16:15 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
 import Portfolio from './pages/Portfolio'
 import Nav from './components/Nav'
-import Wrapper from './components/Wrapper'
+
 
 
 function App() {
@@ -13,10 +13,7 @@ function App() {
       <div className="App">
         <Nav />
         <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exact path='/'>
-            <Redirect to="/home" />
-          </Route>
+          <Route exact path="/" component={Home} />
           <Route exact path={"/portfolio"} component={Portfolio}/>
           <Route exact path={"/contact"} component={Contact}/>            
         </Switch>
