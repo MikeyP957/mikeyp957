5e8d4a3d1801a1bce74b818c1e27c8e8dc277efb
mikeyp957 <peraram2@gmail.com>
2021-07-20 15:03:07 -0700

    add technologies icon and dropdown for description

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from 'react-bootstrap/Card'
 
 import "./style.css";
 
 function ProjectCard(props) {
-  
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpand = () => {
+    let openAndClose = isExpanded;
+    setIsExpanded(!openAndClose)
+  }
   return (
     
       <Card className='projCard' style={{ width: '50rem' }}>
@@ -18,8 +23,15 @@ function ProjectCard(props) {
             <Card.Img className='projImg' variant="top" src={props.image} />       
             </Card.Title>
             <Card.Text>
-
+            <h3>Technologies: </h3>
+              <div className="projIcons">              
+              {props.icons}
+              </div>              
+                <br/>
+                <button onClick={handleExpand}>expand</button>
+              <div className={isExpanded ? 'textDescript active' :'textDescript'}>
               {props.description}
+              </div>              
             </Card.Text>
 
           </Card.Body>
