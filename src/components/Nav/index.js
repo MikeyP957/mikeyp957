77459a4988d9b5f1a8acc5ddbdee4275b36b5069
mikeyp957 <peraram2@gmail.com>
2021-08-13 21:16:07 -0700

    modify styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -16,8 +16,7 @@ class Navbar extends Component {
     return (
       <nav className='NavbarItems'>
        <h1 className= 'navbar-logo' >        
-        Welcome
-        <i className="fas fa-cat"></i> 
+        Welcome        
         </h1>
         <div className = 'menu-icon' onClick ={this.handleClick}>
           <i className ={this.state.clicked ? 'fas fa-times' : 'fas fa-bars' }> </i>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,15 +13,30 @@ class Home extends Component {
     render() {
         return (
             <div className='mainContent'>
-                <Title title={"About Me"} />
                 <Container>
                     <Row>
                         <Col xs={6} md={4}>
-                        <Image className="profPic" align='center' src={profilePic} thumbnail />
+                        <Image className="profPic" align='left' src={profilePic} thumbnail />
+                        </Col>
+                        <Col xs={6} md={4} >                                        
+                            <Title align="center" title={"Hello, my name is Mikey Perara. I am a full-stack developer based in Seattle, Washington."} />
                         </Col>
                     </Row>
-                    <Row>
-                        <Col className='aboutMe' align='center' xs={6} md={4} >
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <Row>                      
+                       
+                        <Col className='aboutMe' align='' xs={6} md={4} >
                             <p>Full stack web developer and math educator with working knowledge of building functions, mapping transformations, and logic. Earned certificate of completion from University of Washington’s Full-Stack Web Development Bootcamp by showing ability in using technologies including JavaScript, Node.js, CSS and HTML. Also certified as a National Boards Teacher by showing evidence of collaborative practices, proficient understanding of mathematics, clear communicator and distinguished teaching practices. Through working with other experienced teachers, engaging in professional development and completing a Master’s degree in teaching, I am experienced in working productively with others and continue to grow professionally. As a mathematician I enjoy solving difficult problems with elegant solutions and refining my skills by learning from others. My curious mind, my understanding of mathematics, my willingness to collaborate and learn from others and problem solving skills make me a strong addition to any team.</p>
                         </Col>
                     </Row>
